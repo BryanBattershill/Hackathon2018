ffd23314186aa867e454b25a0b24b1a2f67335ea
Bryan Battershill <bryan.battershill@gmail.com>
2018-02-17 18:08:08 -0500

    Add files via upload

---
+++ PlayerClass.py
@@ -0,0 +1,16 @@
+class PlayerClass:
+
+    #VARS
+    lives
+    def __init__(self):
+        #CONSTRUCTER
+        lives = 3
+
+    def damage():
+        lives = lives - 1
+        dead()
+        
+    def dead():
+        return lives==0
+
+    
