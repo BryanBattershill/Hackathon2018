a01796b481a85f5692141e4a708a16bdea8c4a42
Bryan Battershill <bryan.battershill@gmail.com>
2018-02-17 19:55:53 -0500

    Add files via upload

--- BossClass.py
+++ BossClass.py
@@ -7,13 +7,17 @@ class BossClass:
         speed = 5
         radius = 5
         position = (0,0)
-    def incSize():
+    def incSize(self):
         size += 1
-    def incSpeed():
+    def incSpeed(self):
         speed+= 1
-    def move(pos):
+    def move(self,pos,PRadius,player):
         dx = pos[0]-position[0]
         dy = position[1]-pos[1]
         direction = math.atan(dy/dx)
-        position[0] += speed*cos(direction)
-        position[1] += speed*sin(direction)
+        position[0] += speed*math.cos(direction)
+        position[1] += speed*math.sin(direction)
+        if math.sqrt(dx**2+dy**2)<=radius+PRadius:
+            player.damage()
+        
+    
--- PlayerClass.py
+++ PlayerClass.py
@@ -1,14 +1,24 @@
+import time
+
 class PlayerClass:
 
     #VARS
     lives
+    buffer
     def __init__(self):
         #CONSTRUCTER
         lives = 3
+        buffer = 0
 
     def damage():
         lives = lives - 1
         dead()
+        if buffer !=0:
+            buffer = time.time() - buffer
+        else:
+            buffer = time.time()
+        if buffer > 2:
+            buffer = 0;
         
     def dead():
         return lives==0
